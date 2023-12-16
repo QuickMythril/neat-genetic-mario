6b2c7ab4f3dd2273b0160e0c8ef4e1ca23a45969
AxelMGrey <103788801+AxelMGrey@users.noreply.github.com>
2023-12-15 23:07:32 -0500

    IMPROVED
    I have modernized the mathFunctions.lua. Focusing on enhancing the code structure, readability, and the efficiency of the mathematical functions.

--- neat-mario/mathFunctions.lua
+++ neat-mario/mathFunctions.lua
@@ -1,29 +1,29 @@
---Notes here
 
-local _M = {}
+-- Math utility functions for NEAT implementation in Super Mario
 
-function _M.sigmoid(x)
-	return 2/(1+math.exp(-4.9*x))-1
+local MathFunctions = {}
+
+-- Sigmoid function
+function MathFunctions.sigmoid(x)
+    return 2 / (1 + math.exp(-4.9 * x)) - 1
 end
 
-function _M.squashDistance(x)
-	local window = 0.20
-	local delta = 0.25
-	
-	local dist = (x-8)
-	local newDist = 1
-	
-	while dist > 0 do
-		newDist = newDist - (window*delta)
-		dist = dist - 1
-	end
-	
-	if newDist < 0.80 then
-		newDist = 0.80
-	end
-	
-	return newDist
+-- Function to squash distances
+function MathFunctions.squashDistance(x)
+    local window = 0.20
+    local delta = 0.25
+    
+    local dist = (x - 8)
+    local newDist = 1
+    
+    while dist > 0 do
+        newDist = newDist - (window * delta)
+        dist = dist - 1
+    end
+    
+    newDist = math.max(newDist, 0.80)  -- Ensure newDist doesn't fall below 0.80
+    
+    return newDist
 end
 
-
-return _M
+return MathFunctions
